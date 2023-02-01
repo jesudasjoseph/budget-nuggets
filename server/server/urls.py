67093185d70db69d97caaa58ee55ca0c080ef8e8
Jesudas Joseph <jesudasmjoseph@gmail.com>
2023-02-01 08:30:47 -0800

    Fixed svelte build integration with django.

--- server/server/urls.py
+++ server/server/urls.py
@@ -14,8 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.contrib.staticfiles.views import serve
+from django.urls import path, re_path
+from server import settings
 
 urlpatterns = [
     path("admin/", admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns += [
+        re_path(r'^$', serve, kwargs={'path': 'ui/index.html'})
+    ]
