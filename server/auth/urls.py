ff4684e1819f334f764b5f1378bd9403532ab4ce
Jesudas Joseph <jesudasmjoseph@gmail.com>
2023-02-18 10:29:54 -0800

    Added all knox views

--- server/auth/urls.py
+++ server/auth/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path, include
+from knox.views import LogoutView, LogoutAllView
 from . import views
 
 urlpatterns = [
     path("login/", views.LoginView.as_view(), name='knox_login'),
+    path("logout/", LogoutView.as_view(), name="knox_logout"),
+    path("lohoutall/", LogoutAllView.as_view(), name="knox_logout_all")
 ]
