16b940507abd4e1130c998f77c83fdf4341d725e
Jesudas Joseph <jesudasmjoseph@gmail.com>
2023-03-12 18:14:55 -0700

    Modified accounts models and migrations

--- server/accounts/models.py
+++ server/accounts/models.py
@@ -1,12 +1,20 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 # Create your models here.
 class Account(models.Model):
-    user = models.ManyToManyField(User, through="Role")
+    user = models.ManyToManyField(User, through="AccountUser")
 
 
-class Role(models.Model):
+class AccountUser(models.Model):
+    Basic = "basic"
+    Owner = "owner"
+    Admin = "admin"
+    Viewer = "viewer"
+
+    Roles = [(Basic, "Basic"), (Owner, "Owner"), (Admin, "Admin"), (Viewer, "Viewer")]
+
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     account = models.ForeignKey(Account, on_delete=models.CASCADE)
-    role = models.CharField(max_length=32)
+    role = models.CharField(max_length=16, choices=Roles, default=Viewer)
