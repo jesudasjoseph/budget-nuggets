3dd25adbf9f1eae39881a573cc9ca8072d0b72f1
Jesudas Joseph <jesudasmjoseph@gmail.com>
2023-03-13 17:19:13 -0700

    Modified Account & User models.

--- server/accounts/views.py
+++ server/accounts/views.py
@@ -3,13 +3,19 @@ from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.authtoken.serializers import AuthTokenSerializer
 from accounts.models import Account
+from user.models import Accounts, User
 from accounts.serializers import AccountSerializer
 
 
 # Create your views here.
 class AccountViewSet(viewsets.ViewSet):
     def list(self, request):
-        queryset = Account.objects.filter(user=request.user)
+        user = User.objects.get(user=request.user)
+        print(user)
+        account_ids = Accounts.objects.filter(user=user)
+        print(account_ids)
+
+        queryset = Account.objects.filter(id_in=account_ids)
         serializer = AccountSerializer(queryset, many=True)
 
         if not serializer.data:
