82eea3033910e377128a324a5f807eb473a9ebc9
Jesudas Joseph <jesudasmjoseph@gmail.com>
2024-01-29 23:14:03 -0800

    Add user field to transaction

--- server/transaction/models.py
+++ server/transaction/models.py
@@ -1,5 +1,10 @@
 from django.db import models
+
+from django.contrib.auth import get_user_model
+
 from budget.models import BudgetCategory, BudgetPeriod
+
+User = get_user_model()
 
 
 class Transaction(models.Model):
@@ -14,6 +19,7 @@ class Transaction(models.Model):
         through_fields=("transaction", "category"),
     )
     budget_period = models.ForeignKey(BudgetPeriod, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
 
 class TransactionCategory(models.Model):
