a0ddc60dfb6629702aa5c8c9bdd661b006a072ed
Jesudas Joseph <jesudasmjoseph@gmail.com>
2024-02-01 18:53:21 -0800

    Add minor period logic to ui

---
+++ ui/src/api/period.ts
@@ -0,0 +1,26 @@
+import { authenticatedAPICall } from './util';
+
+export async function createPeriod(date: Date, budget_id: number) {
+	return authenticatedAPICall(
+		'POST',
+		'period/create/',
+		{
+			date: date.toISOString().split('T', 1)[0],
+			budget: budget_id
+		},
+		true
+	);
+}
+
+export async function getPeriodByID(period_id: number) {
+	return authenticatedAPICall('GET', `period/${period_id}/`, true);
+}
+
+export async function getPeriodByDate(date: Date, budget_id: number) {
+	return authenticatedAPICall(
+		'GET',
+		`period?budget=${budget_id}&date=${date.toISOString().split('T', 1)[0]}`,
+		undefined,
+		true
+	);
+}
--- ui/src/routes/app/budgets/[id=integer]/+page.svelte
+++ ui/src/routes/app/budgets/[id=integer]/+page.svelte
@@ -1,7 +1,16 @@
 <script lang="ts">
 	import { page } from '$app/stores';
 	import { getBudget } from '@api/budget';
+	import { getPeriodByDate, createPeriod } from '@api/period';
 	import { onMount } from 'svelte';
+
+	interface Period {
+		id: number;
+		start_date: Date;
+		end_date: Date;
+		label: string;
+		budget: number;
+	}
 
 	let name = '';
 	let type = '';
@@ -9,12 +18,27 @@
 	let users: number[] = [];
 	let owner: number | undefined = undefined;
 
+	let today = new Date(Date.now());
+
+	let period: Period;
+
+	function createThisBudgetPeriod() {
+		createPeriod(today, id).then((data: Period) => {
+			period = data;
+		});
+	}
+
 	onMount(() => {
 		getBudget(id).then((data) => {
 			name = data.name;
 			type = data.type;
 			users = data.users;
 			owner = data.owner;
+		});
+
+		getPeriodByDate(today, id).then((data: [Period]) => {
+			if (data.length) period = data[0];
+			else createThisBudgetPeriod();
 		});
 	});
 </script>
@@ -24,3 +48,7 @@
 {id}
 {users}
 {owner}
+
+{#if period}
+	{period.label}
+{/if}
